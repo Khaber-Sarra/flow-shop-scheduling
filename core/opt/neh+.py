aeb7cc6a0e9e67ae35f59af71b1bbad4efbe93a9
zacnnnt <zacnnnt@localhost.localdomain>
2021-05-22 15:53:15 +0100

    improved version of NEH added

--- core/opt/neh+.py
+++ core/opt/neh+.py
@@ -1,10 +1,11 @@
 import pandas
 import numpy as np
-from loader import loader
+
 from cmax import cmax
+from loader import loader
 
 
-def neh(data,nb_jobs):
+def neh(data, nb_jobs):
     # TODO add dynamic  nb_machines
     cmax_t = 9999999
     '''
@@ -13,9 +14,8 @@ def neh(data,nb_jobs):
     :) 
     '''
     mem_jobs_list = []
-    partiel_jobs_list, res = [], []
+    partiel_jobs_list, res, tmp_list = [], [], []
     data['jid'] = data.index.copy()
-
 
     data['sum'] = data.drop('jid', axis=1).sum(axis=1)
     # data.sort_values(['sum'],ascending=False,inplace = True)
@@ -29,20 +29,35 @@ def neh(data,nb_jobs):
         cmax_t = 9999999
         for i in range(len(mem_jobs_list) + 1):
             partiel_jobs_list = mem_jobs_list
-            tmp = np.insert(partiel_jobs_list, i, j, axis=0)
-            c = cmax(tmp, nb_jobs)
+            tmp_list = np.insert(partiel_jobs_list, i, j, axis=0)
+            c = cmax(tmp_list, nb_jobs)
             if c < cmax_t:
-                res = tmp
+                res = tmp_list
                 cmax_t = c
         mem_jobs_list = res
     print(mem_jobs_list, cmax_t)
-    return mem_jobs_list, cmax_t
+    """
+    étape 6 de  NEH Amélioré, voir " Amélioration de la NEH " pour plus de détails.
+    """
+    tmp_list =[]
+    for i in range(nb_jobs):
+        for j in range(i+1,nb_jobs):
+            tmp_list = mem_jobs_list.copy()
+            tmp_list[i], tmp_list[j] = mem_jobs_list[j], mem_jobs_list[i]
+            c = cmax(tmp_list, nb_jobs)
+            if c < cmax_t:
+                res = tmp_list
+                cmax_t = c
 
+    mem_jobs_list = res
+
+
+    return mem_jobs_list, cmax_t
 
 
 """
 for testing just uncomment the two lines below 
-and modify the data.txt file
+and modify the data.txt file and the number of jobs
 """
 
 data = loader("../data/data.txt")
